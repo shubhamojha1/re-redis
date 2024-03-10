cbb35239a6008a401ca142e049b3edf980e61730
Shubham <subham.k.ojha@gmail.com>
2024-03-11 02:05:06 +0530

    Minor changes

--- client.cpp
+++ client.cpp
@@ -1,6 +1,8 @@
 // cmd: 
 // gcc -o client client.cpp -lws2_32 -lwsock32 -L $MinGW\lib; .\client
-#define _WIN32_WINNT 0x0600
+// g++ -I%BOOST_ROOT% -L%BOOST_ROOT%\stage\lib -o client client.cpp -lwsock32 -lws2_32; ./client
+
+#define _WIN32_WINNT _WIN32_WINNT_WIN10 // 0x0600
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -9,6 +11,9 @@
 #include <errno.h>
 #include <unistd.h>
 #include <assert.h>
+#include <string>
+#include <vector>
+#include <iostream>
 // For Unix/Linux
 // #include <arpa/inet.h> 
 // #include <sys/socket.h>
@@ -21,7 +26,7 @@
 // for networking functionality in Windows
 #pragma comment(lib, "Ws2_32.lib")
 
-const size_t K_MAX_MSG = 4096;
+
 
 // static void die(const char *msg){
 //     int err = errno;
@@ -42,9 +47,20 @@ static void die(const char *msg){
 }
 
 static int32_t read_full(SOCKET fd, char *buf, size_t n) { // (int fd)
+
+    printf("inside read_full()\n");
     while (n > 0) {
         // ssize_t rv = read(fd, buf, n);
+        // int rv = recv(fd, buf, n, 0);
+
+        printf("Attempting to receive from server...\n", n);
         int rv = recv(fd, buf, n, 0);
+        if (rv < 0) {
+            printf("send() failed with error: %d\n", WSAGetLastError());
+        } else {
+            printf("Successfully sent %d bytes to server.\n", rv);
+        }
+
         if (rv <= 0) {
             return -1; // error or unexpected EOF
         }
@@ -56,18 +72,42 @@ static int32_t read_full(SOCKET fd, char *buf, size_t n) { // (int fd)
 }
 
 static int32_t write_all(SOCKET fd, const char *buf, size_t n) { // (int fd)
+
+    printf("inside write_all()\n");
+    // while (*buf != '\0') {
+    //     printf("%c", *buf);
+    //     ++buf;
+    // }
+
     while (n > 0){
         // ssize_t rv = write(fd, buf, n);
+        // int rv = send(fd, buf, n, 0);
+        // printf("Sending to server: %.*s", *buf);
+        // std::cout << buf << "\n";
+
+        printf("Attempting to send %zu bytes to server...\n", n);
         int rv = send(fd, buf, n, 0);
+        if (rv < 0) {
+            printf("send() failed with error: %d\n", WSAGetLastError());
+        } else {
+            printf("Successfully sent %d bytes to server.\n", rv);
+        }
+
         if (rv <= 0) {
             return -1; // error
         }
         assert((size_t)rv <= n);
         n -= (size_t)rv;
         buf += rv;
+
+        printf("size of n: %d\n", n);
+        printf("size of rv: %d\n", rv);
+        printf("size of buf (pointer): %d", buf);
     }
     return 0;
 }
+
+const size_t K_MAX_MSG = 4096;
 
 // static int32_t query(SOCKET fd, const char *text) {
 //     uint32_t len = (uint32_t)strlen(text);
@@ -128,13 +168,15 @@ static int32_t write_all(SOCKET fd, const char *buf, size_t n) { // (int fd)
 //     return write_all(fd, wbuf, 4 + len);
 // }
 
-static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
+static int32_t send_req(SOCKET fd, const std::vector<std::string> &cmd) {
+
+    printf("inside send_req()\n");
     uint32_t len = 4;
     for (const std::string &s : cmd) {
-        len += 4 + s.size();
+        len += 4 + s.size(); // adding 4, to accomodate for header
     }
 
-    if(len > K_MAX_MSG)
+    if(len > K_MAX_MSG) // if greater than MAX ALLOWED
         return -1;
 
     char wbuf[4 + K_MAX_MSG];
@@ -143,7 +185,7 @@ static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
     memcpy(&wbuf[4], &n, 4);
     size_t cur = 8;
 
-    for(const std::string &s: cmd){
+    for(const std::string &s: cmd){ // filling up wbuf
         uint32_t p = (uint32_t)s.size();
         memcpy(&wbuf[cur], &p, 4);
         memcpy(&wbuf[cur + 4], s.data(), s.size());
@@ -153,9 +195,10 @@ static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
     return write_all(fd, wbuf, 4 + len);
 }
 
-static int32_t read_res(int fd) {
+static int32_t read_res(SOCKET fd) {
     // 4 bytes header
     char rbuf[4 + K_MAX_MSG + 1];
+    printf("inside read_res\n");
     errno = 0;
     int32_t err = read_full(fd, rbuf, 4);
     if (err) {
@@ -166,6 +209,7 @@ static int32_t read_res(int fd) {
         }
         return err;
     }
+    printf("read_full works fine\n");
 
     uint32_t len = 0;
     memcpy(&len, rbuf, 4);  // assume little endian
@@ -193,11 +237,12 @@ static int32_t read_res(int fd) {
         return -1;
     }
     memcpy(&rescode, &rbuf[4], 4);
+    printf("just before server says\n");
     printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
     return 0;
 }
 
-int main() {
+int main(int argc, char **argv) {
     // int fd = socket(AF_INET, SOCK_STREAM, 0);
     // if (fd < 0){
     //     die("socket()");
@@ -292,20 +337,39 @@ int main() {
     // return 0;
 
     // multiple pipelined requests
-    const char *query_list[3] = {"hello1", "hello2", "hello3"};
-    for(ssize_t i = 0; i < 3; i++) {
-        int32_t err = send_req(fd, query_list[i]);
-        if (err) { 
-            goto L_DONE;
-        }
-        printf("Sent message: %s\n", query_list[i]); // Debugging statement
+    // const char *query_list[3] = {"hello1", "hello2", "hello3"};
+    // for(ssize_t i = 0; i < 3; i++) {
+    //     int32_t err = send_req(fd, query_list[i]);
+    //     if (err) { 
+    //         goto L_DONE;
+    //     }
+    //     printf("Sent message: %s\n", query_list[i]); // Debugging statement
+    // }
+    // for (size_t i = 0; i < 3; i++) {
+    //     int32_t err = read_res(fd);
+    //     if (err) {
+    //         goto L_DONE;
+    //     }
+    // }
+
+    // new -> 
+
+    std::vector<std::string> cmd;
+    for(int i=1;i<argc;i++)
+        cmd.push_back(argv[i]);
+
+    printf("printing cmd vector args---> \n");
+    for(int i=0;i<cmd.size();i++){
+        printf("%s\n", cmd[i].c_str());
     }
-    for (size_t i = 0; i < 3; i++) {
-        int32_t err = read_res(fd);
-        if (err) {
-            goto L_DONE;
-        }
-    }
+    printf(">>>>>\n");
+    int32_t err = send_req(fd, cmd);
+    if (err)
+        goto L_DONE;
+
+    err = read_res(fd);
+    if (err)
+        goto L_DONE;
 L_DONE:
     closesocket(fd);
     return 0;
